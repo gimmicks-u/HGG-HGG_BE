32f82c951c3c7095780306d1f8d30afae61859dd
gimmicks-u <is@pusan.ac.kr>
2022-10-11 00:09:49 +0900

    [feat] login API 추가
    JWT방식 api추가
    cors, cookieparser, jwt 패키지 추가

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -3,7 +3,7 @@ import { UsersController } from '../controllers/users.controller';
 
 const router = Router();
 
-//회원가입
+// 회원가입
 router.post('/', UsersController.createUser);
 
 export default router;
