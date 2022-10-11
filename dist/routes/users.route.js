126281ce2808191041d1efa34cee73b11d5c354b
InGukOh <87802273+InGukOh@users.noreply.github.com>
2022-10-11 23:42:07 +0900

    dist 삭제

--- dist/routes/users.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var users_controller_1 = require("../controllers/users.controller");
-var router = (0, express_1.Router)();
-router.post('/', users_controller_1.UsersController.createUser);
-exports.default = router;
-//# sourceMappingURL=users.route.js.map
